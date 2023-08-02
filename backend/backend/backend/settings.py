62930475a421fce3cd8e174b0e07e531588bf538
YouweiPeng <98921666+YouweiPeng@users.noreply.github.com>
2023-08-02 15:01:52 -0600

    database configs

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -80,13 +80,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'healing',
+        'USER': 'root',
+        'PASSWORD': '990907PYWpyw.',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
